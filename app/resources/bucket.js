d77b944b78f2b85d98b90a0caee7f3110b46753d
Mark Nicola <mnicola@mac-mark-macbook.local>
2015-05-19 10:27:01 -0700

    switch from total.js to basic mean stack

--- app/resources/bucket.js
+++
@@ -1,5 +0,0 @@
-app.factory('Bucket', function($resource) {
-  return $resource('/bucket/:id/', { id: '@id' });
-}).factory('Image', function($resource) {
-  return $resource('/bucket/:id/:key/img/');
-});
