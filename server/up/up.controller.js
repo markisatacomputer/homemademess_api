89b8fa1e072f227eb98def43992851fdfe91fb24
Mark Nicola <mark.is.at.a.computer@gmail.com>
2015-05-26 13:17:18 -0700

    image exif data

--- server/up/up.controller.js
+++ server/up/up.controller.js
@@ -1,11 +1,36 @@
 'use strict';
 
 var _ = require('lodash');
-var multer  = require('multer')
+var aws = require('aws-sdk');
+aws.config.endpoint = process.env.AWS_ENDPOINT;
+var dreamObjects = new aws.S3();
 var Image = require('../api/image/image.model');
+var ExifImage = require('exif').ExifImage;
+var Q = require('q');
 
-// Get list of images
+
+function getExif(image) {
+  var deferred = Q.defer();
+  try {
+    new ExifImage({ image : image}, function (error, exifData) {
+      if (error)
+        console.log('Error: '+ error.message);
+      else
+        deferred.resolve(exifData);
+    });
+  } catch (error) {
+    console.log('Error: ' + error.message);
+    deferred.reject(error);
+  }
+  return deferred.promise;
+}
+
+
+// Process upload
 exports.index = function(req, res) {
-  console.log(req.files);
-  return res.json(200, req.files);
+  var file = req.files.file;
+  console.log(file);
+  getExif(file.path).then(function (exif){
+    return res.json(200, exif);
+  });
 };
