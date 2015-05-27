acd129f585594dad60d08e15671badae89b1297c
Mark Nicola <mark.is.at.a.computer@gmail.com>
2015-05-26 20:05:20 -0700

    update image model

--- server/api/image/image.model.js
+++ server/api/image/image.model.js
@@ -1,12 +1,23 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+  Schema = mongoose.Schema;
+
+var ObjectId = mongoose.Schema.Types.ObjectId
 
 var ImageSchema = new Schema({
   name: String,
-  info: String,
-  active: Boolean
+  filename: String,
+  tags: [{
+    Type: String
+  }],
+  exif: [{
+    Type: ObjectId
+  }],
+  original: String,
+  derivative:[{
+    Type: ObjectId
+  }]
 });
 
 module.exports = mongoose.model('Image', ImageSchema);
