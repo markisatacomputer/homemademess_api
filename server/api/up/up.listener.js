1555bf5885f450fea2957ba12c45cb907d2c81a6
Mark Nicola <mark.is.at.a.computer@gmail.com>
2015-06-19 16:35:03 -0700

    api comment about up.listener logic

--- server/api/up/up.listener.js
+++ server/api/up/up.listener.js
@@ -1,5 +1,10 @@
 /**
  *   Pass all S3Upload events to socket.io
+ *
+ *     This way we use TWO listeners on ONE event emitter 
+ *     to pass events on to the socket from countless event 
+ *     listeners that each Upload object place on their individual 
+ *     S3 Managed Upload instance.
  */
 
 'use strict';
