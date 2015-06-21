5699cfabdc852d652212e7530bce7aac1d72025b
Mark Nicola <mark.is.at.a.computer@gmail.com>
2015-06-21 02:20:06 -0700

    delete derivatives from S3 on db delete Image

--- server/api/image/image.model.js
+++ server/api/image/image.model.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var _ = require('lodash');
+var aws = require('aws-sdk');
+aws.config.endpoint = process.env.AWS_ENDPOINT;
+var url = require('url');
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
@@ -38,6 +42,24 @@ var ImageSchema = new Schema({
     type: Number,
     default: Date.now()
   }
+});
+
+ImageSchema.post('remove', function (doc) {
+  // remove all derivatives
+  var ds = doc.derivative;
+  var s3 = new aws.S3();
+  _.forEach(ds, function(d){
+    var path = url.parse(d.uri).pathname.slice(1);
+    //  remove from s3 bucket
+    var params = {
+      Bucket: process.env.AWS_THUMB_BUCKET,
+      Key: path,
+    }
+    s3.deleteObject(params, function(err, data){
+      if(err) { console.log('image:remove error', err, path); }
+      console.log('image:remove derivative', path);
+    });
+  });
 });
 
 module.exports = mongoose.model('Image', ImageSchema);
