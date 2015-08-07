59c56edc9892bc1c040ddfb21bb22bda24389f69
Mark Nicola <marknicola@BWSF-MNicola.local>
2015-08-07 10:40:12 -0700

    change routes

--- server/routes.js
+++ server/routes.js
@@ -8,18 +8,17 @@ var errors = require('./components/errors');
 
 module.exports = function(app) {
 
-  // Insert routes below
-  app.use('/api/images', require('./api/image'));
-  app.use('/api/tags', require('./api/tag'));
-  app.use('/api/auto', require('./api/auto'));
-  app.use('/api/things', require('./api/thing'));
-  app.use('/api/users', require('./api/user'));
-  
-  // This might need to be moved out of api for clarity
+  // API routes
+  app.use('/images', require('./api/image'));
+  app.use('/tags', require('./api/tag'));
+  app.use('/auto', require('./api/auto'));
+  app.use('/things', require('./api/thing'));
+  app.use('/users', require('./api/user'));
   app.use('/up', require('./api/up'));
 
+  //  Auth
   app.use('/auth', require('./auth'));
-
+  //  Github Webhook
   app.use('/postrecieve', require('./githubhook'));
 
   // All other routes should return an error
