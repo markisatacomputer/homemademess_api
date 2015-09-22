40cb2e25b949f2a067724c2ec67d7bdf87288ba7
Mark Nicola <mark.is.at.a.computer@gmail.com>
2015-09-21 20:36:04 -0400

    autocomplete search case insensitive

--- server/api/auto/auto.controller.js
+++ server/api/auto/auto.controller.js
@@ -5,7 +5,7 @@ var Tag = require('../tag/tag.model');
 
 exports.autocomplete = function(req, res) {
   var q = _.values(req.query);
-  var re = new RegExp(q.join(''), "g");
+  var re = new RegExp(q.join(''), "gi");
   Tag.find({text: {$regex: re}}, null, { $orderby: { text : -1 } }, function(err, tags){
     if(err) { return res.json({status: 500}); }
     if(!tags) { return res.json({status:404}); }
