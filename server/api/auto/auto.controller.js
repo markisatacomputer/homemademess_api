5e162f96a96734ae08e6d71dbda31f3091f9c8ae
Mark Nicola <mark.is.at.a.computer@gmail.com>
2015-09-21 20:27:18 -0400

    order autocomplete by text alpha

--- server/api/auto/auto.controller.js
+++ server/api/auto/auto.controller.js
@@ -6,7 +6,7 @@ var Tag = require('../tag/tag.model');
 exports.autocomplete = function(req, res) {
   var q = _.values(req.query);
   var re = new RegExp(q.join(''), "g");
-  Tag.find({text: {$regex: re}}, function(err, tags){
+  Tag.find({text: {$regex: re}}, null, { $orderby: { text : -1 } }, function(err, tags){
     if(err) { return res.json({status: 500}); }
     if(!tags) { return res.json({status:404}); }
     //  no repeats in our results please... there shouldn't be any anyway
