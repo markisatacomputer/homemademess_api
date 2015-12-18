316f3c0a608f3a16f3e81352aa3df16314808288
Mark Nicola <mark.is.at.a.computer@gmail.com>
2015-12-18 14:51:59 -0800

    add Image ref to Tag model

--- server/api/tag/tag.model.js
+++ server/api/tag/tag.model.js
@@ -2,10 +2,15 @@
 
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId
 
 var TagSchema = new Schema({
   text: String,
-  namespace: String
+  namespace: String,
+  _images: [{
+    type: ObjectId,
+    ref: 'Image'
+  }]
 });
 
 module.exports = mongoose.model('Tag', TagSchema);
