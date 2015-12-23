c95ec93bbfc5a7e15ce2692af6a6d909e641e526
Mark Nicola <mark.is.at.a.computer@gmail.com>
2015-12-23 15:59:16 -0800

    add sort fields to tag model

--- server/api/tag/tag.model.js
+++ server/api/tag/tag.model.js
@@ -10,7 +10,23 @@ var TagSchema = new Schema({
   _images: [{
     type: ObjectId,
     ref: 'Image'
-  }]
+  }],
+  _sort: {
+    size: {
+      type: Number,
+      default: 0
+    },
+    naturalized: {
+      type: String,
+      Lowercase: true
+    }
+  }
+});
+
+TagSchema.pre('save', function (next) {
+  // set sort field
+  this._sort.naturalized = this.text.toLowerCase();
+  next();
 });
 
 module.exports = mongoose.model('Tag', TagSchema);
