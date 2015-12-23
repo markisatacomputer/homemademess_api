e9dc65df88601d375bbb1e81ab82715d7e123a39
Mark Nicola <mark.is.at.a.computer@gmail.com>
2015-12-23 15:59:59 -0800

    use sort fields from tag model in auto

--- server/api/auto/auto.controller.js
+++ server/api/auto/auto.controller.js
@@ -6,18 +6,13 @@ var Tag = require('../tag/tag.model');
 exports.autocomplete = function(req, res) {
   var q = _.values(req.query);
   var re = new RegExp(q.join(''), "gi");
-  Tag.find({text: {$regex: re}, _images: {$exists: true}}, null, { sort: { text : 1 } }, function(err, tags){
-    if(err) { return res.json({status: 500}); }
-    if(!tags) { return res.json({status:404}); }
-    //  no repeats in our results please... there shouldn't be any anyway
-    var tagstexts = {};
-    var returntags = [];
-    _.forEach(tags, function(tag, i) {
-      if (!tagstexts.hasOwnProperty(tag.text)) {
-        returntags.push(tag);
-      }
-      tagstexts[tag.text] = tag.text;
+  Tag.find({text: {$regex: re}, '_sort.size': {$gt: 0}}, 
+    null, 
+    { sort: { '_sort.size': -1, '_sort.naturalized' : 1 }, limit: 10 }, 
+    function (err, tags){
+      if(err) { return res.json({status: 500}); }
+      if(!tags) { return res.json({status:404}); }
+      //  sort tags again - mongo can't do case insensitive
+      return res.json(tags);
     });
-    return res.json(returntags);
-  });
 }
