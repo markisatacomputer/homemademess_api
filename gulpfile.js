4d19bb9db0441c5fd247af10985196f7920de0c4
Mark Nicola <mark.is.at.a.computer@gmail.com>
2017-01-01 10:46:28 -0800

    use gulp

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+/**
+ *  Welcome to your gulpfile!
+ *  The gulp tasks are splitted in several files in the gulp directory
+ *  because putting all here was really too long
+ */
+
+'use strict';
+
+var gulp = require('gulp')
+  , nodemon = require('gulp-nodemon')
+  , jshint = require('gulp-jshint')
+
+gulp.task('lint', function () {
+  gulp.src('server/**/*.js')
+    .pipe(jshint())
+})
+
+gulp.task('develop', function () {
+  var stream = nodemon({
+      script: 'server/app.js'
+    , ext: 'js'
+    , tasks: ['lint']
+    , watch: ['server/']
+    , delay: 3
+    , env: {
+        NODE_ENV: "development",
+        PORT: "8666"
+      }
+    })
+
+  stream
+      .on('restart', function () {
+        console.log('restarted!')
+      })
+      .on('crash', function() {
+        console.error('Application has crashed!\n')
+         stream.emit('restart', 10)  // restart the server in 10 seconds
+      })
+})
