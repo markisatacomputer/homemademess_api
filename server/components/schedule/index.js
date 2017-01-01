513dcd7ae80b9bc24d5a9a37c2494b0320a24769
Mark Nicola <mark.is.at.a.computer@gmail.com>
2017-01-01 10:58:31 -0800

    update old image base uri

--- server/components/schedule/index.js
+++ server/components/schedule/index.js
@@ -6,7 +6,49 @@ var aws = require('aws-sdk');
 aws.config.endpoint = process.env.AWS_ENDPOINT;
 
 var rule = new schedule.RecurrenceRule();
-rule.minute = 13;
+rule.minute = 28;
+
+
+
+//  This is for me when I need it...
+var truncate = function() {
+  var params = {Bucket: 'hmmtesting'};
+  var s3 = new aws.S3();
+  s3.listObjects(params, function(err, data) {
+    console.log(err);
+    _.forEach(data.Contents, function(obj, key){
+      params.Key = obj.Key;
+      s3.deleteObject(params, function(err, data){
+        console.log(err, data);
+      });
+    });
+  });
+}
+
+//  DreamObjects is moving
+var transformBase = function() {
+  Image.find({}, function(err, docs){
+    _.forEach(docs, function(doc, key){
+      var oldBase = 'objects.dreamhost.com',
+      newBase = 'objects-us-west-1.dream.io',
+      derivs = doc.derivative;
+
+      _.forEach(derivs, function(deriv, key) {
+        var uri = derivs[key].uri.replace(oldBase, newBase);
+        derivs[key].uri = uri;
+      });
+      console.log(derivs);
+
+      doc.derivative = derivs;
+      doc.save();
+
+    });
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+//transformBase();  //uncomment to update derivative uris
 
 var j = schedule.scheduleJob(rule, function(){
   //  Remove images that are temporary and the countdown has passed
@@ -24,21 +66,7 @@ var j = schedule.scheduleJob(rule, function(){
       console.log(err);
     }
   });
-});
 
-//  This is for me when I need it...
-var truncate = function() {
-  var params = {Bucket: 'hmmtesting'};
-  var s3 = new aws.S3();
-  s3.listObjects(params, function(err, data) {
-    console.log(err);
-    _.forEach(data.Contents, function(obj, key){
-      params.Key = obj.Key;
-      s3.deleteObject(params, function(err, data){
-        console.log(err, data);
-      });
-    });
-  });
-}
+});
 
 module.exports = j;
