e5cfba409e8dc4e470f2688b8bab813f51e64e3f
Mark Nicola <mark.is.at.a.computer@gmail.com>
2017-05-15 11:49:06 -0700

    add auth to up method

--- server/api/up/index.js
+++ server/api/up/index.js
@@ -2,9 +2,10 @@
 
 var express = require('express');
 var controller = require('./up.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.post('/', controller.index);
+router.post('/', auth.hasRole('admin'), controller.index);
 
 module.exports = router;
