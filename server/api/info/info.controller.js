9ef9636d739215d16206019acbd4c54756d9fda2
Mark Nicola <mark.is.at.a.computer@gmail.com>
2017-09-04 16:31:31 -0700

    info/up - return all dates of upload

--- server/api/info/info.controller.js
+++ server/api/info/info.controller.js
@@ -7,7 +7,7 @@ var Q = require('q');
 
 /**
  *   return lower and upper limits for image dates
- * @return {Object}
+ *   @return {Object}
  */
 var getDateConstraints = function() {
   var resolveDates = Q.defer(),
@@ -25,12 +25,66 @@ var getDateConstraints = function() {
 
   return resolveDates.promise;
 }
+/**
+ *   return unique dates of upload with a granularity of one calendar day
+ *   @return {Array}
+ */
+var getUploadDates = function() {
+  var resolveDates = Q.defer(),
+  dates = {};
+
+  Image.aggregate([
+    //  -  reduce uploadDate to day
+    { $project: {
+      uploadDate: 1,
+      upload: {
+        $floor: {
+          $multiply: [
+            86400000,
+            {
+              $ceil: {
+                $divide: ['$uploadDate', 86400000]
+              }
+            }
+          ]
+        }
+      }
+    }},
+    //  -  only show unique day values
+    { $group: {
+      _id: '$upload'
+      }
+    },
+    //  -  do not return 0.  do we need this?
+    //{ $match : { upload : { $ne: 0 } } },
+    //  -  sort!
+    {
+      $sort: {
+        _id: -1
+      }
+    }
+
+  ]).exec(function(err, dates) {
+    if (err) { resolveDates.reject(err); }
+    resolveDates.resolve(dates);
+  });
+
+  return resolveDates.promise;
+}
 
 // Get a single image
 exports.show = function(req, res) {
   switch (req.params.id) {
     case 'dates':
       getDateConstraints().then(function(dates){
+        return res.json(200, dates);
+      },
+      function(err){
+        return res.json(500, err);
+      });
+      break;
+    case 'up':
+      getUploadDates().then(function(dates){
         return res.json(200, dates);
       },
       function(err){
