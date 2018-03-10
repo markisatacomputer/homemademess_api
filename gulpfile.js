3449aab8fcdbd7b510661be58ba038076537057d
Mark N <mark.is.at.a.computer@gmail.com>
2018-03-10 14:00:29 -0800

    gulp - enable local tasks in gulp/ folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 /**
- *  Welcome to your gulpfile!
- *  The gulp tasks are splitted in several files in the gulp directory
- *  because putting all here was really too long
+ *
+ *
  */
 
 'use strict';
@@ -9,26 +8,56 @@
 var gulp = require('gulp')
   , nodemon = require('gulp-nodemon')
   , jshint = require('gulp-jshint')
-  , localEnv = require('./server/config/local.env.js')
   , _ = require('lodash')
   , env = require('gulp-env')
+  , fs = require('fs')
   , exec = require('child_process').exec
+  , spawn = require('child_process').spawn
+  , spawnProcess
+  , taskfiles
+
+spawnProcess = function(command, args) {
+  var p;
+
+  fs.open('.env.json', 'r', (err, fd) => {
+    if (!err) {
+      env({
+        file: '.env.json'
+      })
+    } else {
+      console.log(err);
+    }
+
+    p = spawn(command, args)
+    p.stdout.on('data', function(data){
+      console.log(data.toString());
+    })
+    p.stderr.on('data', function(data){
+      console.log('error: ' + data.toString());
+    })
+    p.on('exit', function(code){
+      //console.log('child process exited with code ' + code.toString());
+    })
+  });
+}
+
+//  Specific scripts from gulp dir
+taskfiles = fs.readdirSync('gulp')
+taskfiles.forEach( (filename) => {
+  var file = filename.split('.');
+
+  if (file[1] == 'js') {
+    gulp.task( file[0], function() {
+      spawnProcess('node', ['gulp/'+filename])
+    })
+  }
+})
+
+
 
 gulp.task('lint', function () {
   gulp.src('server/**/*.js')
     .pipe(jshint())
-})
-
-gulp.task('seed', function () {
-  env({
-    file: '.env'
-  })
-
-  exec('node server/config/seed.js', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  })
 })
 
 gulp.task('develop', function () {
